ebd8432e80bb3076a5f08d8c2f1b8e024d18901a
Sriram Patil <spsrirampatil@gmail.com>
2014-04-29 23:11:56 +0530

    Added method freeze, freezeecision and freezeRecovery. Declared reuired methods too.

--- lflist/Chunk.hpp
+++ lflist/Chunk.hpp
@@ -49,7 +49,7 @@ private:
 	std::atomic<long> counter;		// number of entries currently allocated
 	Entry *head;
 	Entry *entriesArray;
-	Chunk *newChunk;
+	std::atomic<Chunk *> newChunk;
 	std::atomic<long> mergeBuddy;			// merge ptr and freeze state
 	std::atomic<Chunk *> nextChunk;
 
@@ -84,7 +84,6 @@ public:
 	}
 
 	virtual ~Chunk() {
-		delete[] entriesArray;
 	}
 
 	/*
@@ -145,6 +144,188 @@ public:
 	bool compareAndSetMergeBuddyAndFreezeState(long oldMergeBuddy,
 			long newMergeBuddy) {
 		return mergeBuddy.compare_exchange_strong(oldMergeBuddy, newMergeBuddy);
+	}
+
+	/*
+	 * Goes over all reachable entries in Chunk c, counts them, and returns the
+	 * number of entries. Chunk c is assumed to be frozen and thus cannot be
+	 * modified.
+	 */
+	int getEntrNum(Chunk *chunk) {
+		Entry *entr = chunk->head->nextEntry.load();
+		int count = 0;
+
+		while (Entry::clearFrozen((long) entr) != 0) {
+			count++;
+			entr = entr->nextEntry.load();
+		}
+
+		return count;
+	}
+
+	/*
+	 * Goes over all reachable entries in the old chunk linked list and copies
+	 * them to the new chunk linked list. It is assumed no other thread is
+	 * modifying the new chunk, and that the old chunk is frozen, so it cannot
+	 * be modified as well.
+	 */
+	static void copyToOneChunk(Chunk *oldChunk, Chunk *chunk) {
+		Entry *entr = (Entry *) Entry::clearFrozen(
+				(long) oldChunk->head->nextEntry.load());
+		chunk->head->nextEntry.store(entr);
+
+		Entry *prevEntr = chunk->head;
+		int index = 1;
+
+		while (entr != NULL) {
+			chunk->entriesArray[index] = entr;
+			prevEntr->nextEntry.store((long) entr);
+			prevEntr = entr;
+			entr = (Entry *) Entry::clearFrozen(entr->nextEntry.load());
+			index++;
+		}
+	}
+
+	/*
+	 * Goes over all reachable entries on the old1 and old2 chunks linked lists
+	 * (which are sequential and have enough entries to fill one chunk’s linked
+	 * list) and copies them to the new chunk linked list. It is assumed that no
+	 * other thread modifies the new chunk and that the old chunks are frozen
+	 * and thus don’t change.
+	 */
+	static void mergeToOneChunk(Chunk *old1, Chunk *old2, Chunk *new1) {
+		Entry *entr = (Entry *) Entry::clearFrozen(
+				(long) old1->head->nextENtry.load());
+		new1->head->nextEntry.store(entr);
+
+		Entry *prevEntr = new1->head;
+		int index = 1;
+		while (entr != NULL) {
+			new1->entriesArray[index] = entr;
+			prevEntr->nextEntry.store((long) entr);
+			prevEntr = entr;
+			entr = (Entry *) Entry::clearFrozen(entr->nextEntry.load());
+			index++;
+		}
+
+		entr = Entry::clearFrozen(old2->head->next);
+		while (entr != NULL) {
+			new1->entriesArray[index] = entr;
+			prevEntr->nextEntry.store((long) entr);
+			prevEntr = entr;
+			entr = (Entry *) Entry::clearFrozen((long) entr->nextEntry.load());
+			index++;
+		}
+	}
+
+	/*
+	 * Goes over all reachable entries on the old chunk linked list, finds the
+	 * median key (which is returned) and copies the bellow-median-value keys
+	 * to the new1 chunk and the above-median-value keys to the new2 chunk.
+	 * In addition it sets the new1 chunk’s pointer nextChunk to point at
+	 * the new2 chunk. It is assumed that no other thread is modifying the
+	 * new1 and new2 chunks, and that the old chunk is frozen and cannot be
+	 * modified.
+	 */
+	static long splitIntoTwoChunks(Chunk *old, Chunk *new1, Chunk *new2) {
+		Entry *slowPtr = (Entry *) Entry::clearFrozen(
+				(long) old->head->nextEntry.load());
+		Entry *fastPtr = slowPtr;
+		Entry *fastPtrNext = (Entry *) Entry::clearFrozen(
+				(long) fastPtr->nextEntry.load());
+
+		while (fastPtr != NULL && fastPtrNext != NULL) {
+			fastPtr = (Entry *) Entry::clearFrozen(
+					(long) fastPtrNext->nextEntry.load());
+			fastPtrNext = (Entry *) Entry::clearFrozen(
+					(long) fastPtr->nextEntry.load());
+			slowPtr = (Entry *) Entry::clearFrozen(
+					(long) slowPtr->nextEntry.load());
+		}
+
+		Entry *median = slowPtr;
+
+		Entry *entr = (Entry *) Entry::clearFrozen(
+				(long) old->head->nextEntry.load());
+		Entry *prevEntr = new1->head;
+		int index = 1;
+		while (entr != median) {
+			new1->entriesArray[index] = entr;
+			prevEntr->nextEntry.store((long) entr);
+			prevEntr = entr;
+			entr = (Entry *) Entry::clearFrozen((long) entr->nextEntry.load());
+			index++;
+		}
+
+		entr = (Entry *) Entry::clearFrozen((long) median->nextEntry.load());
+		index = 1;
+		prevEntr = new2->head;
+		while (entr != NULL) {
+			new2->entriesArray[index] = entr;
+			prevEntr->nextEntry.store((long) entr);
+			prevEntr = entr;
+			entr = (Entry *) Entry::clearFrozen((long) entr->nextEntry.load());
+			index++;
+		}
+
+		return median->getKey();
+	}
+
+	/*
+	 * Goes over all reachable entries in the old1 and old2 chunks linked lists
+	 * (which are sequential), finds the median key (which is returned) and
+	 * copies the bellow-median-value keys to the new1 chunk linked list and
+	 * the above-median-value keys to the new2 chunk linked list. In addition
+	 * it sets the new1 chunk’s pointer nextChunk to point to the new2 chunk.
+	 * It is assumed that no other thread modifies the new1 and new2 chunks,
+	 * and that the old chunks are frozen and thus cannot be modified as well.
+	 */
+	static long mergeToTwoChunks(Chunk *old1, Chunk *old2, Chunk *new1,
+			Chunk *new2) {
+		Entry *newListHead = (Entry *) Entry::clearFrozen(
+				(long) old1->head->nextEntry.load());
+		Entry *entr = newListHead, *listNode = newListHead;
+
+		while (entr != NULL) {
+			entr = (Entry *) Entry::clearFrozen((long) entr->nextEntry.load());
+			listNode->nextEntry.store((long) entr);
+		}
+
+		entr = (Entry *) Entry::clearFrozen(
+				(long) old2->head->nextEntry.load());
+		listNode->nextEntry.store((long) entr);
+		while (entr != NULL) {
+			entr = (Entry *) Entry::clearFrozen((long) entr->nextEntry.load());
+			listNode->nextEntry.store((long) entr);
+		}
+
+		Entry *slowPtr = newListHead;
+		Entry *fastPtr = slowPtr;
+		Entry *fastPtrNext = fastPtr->nextEntry.load();
+
+		while (fastPtr != NULL && fastPtrNext != NULL) {
+			fastPtr = fastPtrNext->nextEntry.load();
+			fastPtrNext = fastPtr->nextEntry.load();
+			slowPtr = slowPtr->nextEntry.load();
+		}
+
+		Entry *median = slowPtr;
+		listNode = newListHead;
+		int index = 1;
+		while (listNode != median) {
+			new1->entriesArray[index] = listNode;
+			listNode = listNode->nextEntry.load();
+			index++;
+		}
+
+		listNode = median->nextEntry.load();
+		while (listNode != NULL) {
+			new2->entriesArray[index] = listNode;
+			listNode = listNode->nextEntry.load();
+			index++;
+		}
+
+		return median->getKey();
 	}
 
 	bool search(long key, TData data) {
@@ -214,13 +395,13 @@ public:
 
 		// look for some empty entry
 		for (int i = 0; i < MAX; i++) {
-			if (entriesArray[i].keyData == expectedKeyData) {
-				uint128 oldKeyData = entriesArray[i].keyData;
+			if (chunk->entriesArray[i].keyData == expectedKeyData) {
+				uint128 oldKeyData = chunk->entriesArray[i].keyData;
 				if (oldKeyData
 						== Utils::InterlockedCompareExchange128(
-								&(entriesArray[i].keyData), oldKeyData,
+								&(chunk->entriesArray[i].keyData), oldKeyData,
 								newKeyData))
-					return entriesArray[i];
+					return chunk->entriesArray[i];
 			}
 		}
 
@@ -289,28 +470,120 @@ public:
 	void incCount(Chunk *chunk);bool find(Chunk *chunk, long key);
 
 	void markChunkFrozen(Chunk *chunk) {
-		for (int i = 0; i < chunk->counter.load(); i++) {
-			long savedNext = (uint128) entriesArray[i].nextEntry.load();
+		// Not sure if this should be till MAX or counter
+		for (int i = 0; i < MAX; i++) {
+			long savedNext = (uint128) chunk->entriesArray[i].nextEntry.load();
 			while (!Entry::isFrozen(savedNext)) {
-				entriesArray[i].nextEntry.compare_exchange_strong(savedNext,
-						(long) Entry::markFrozen(savedNext));
-				savedNext = entriesArray[i].nextEntry.load();
+				chunk->entriesArray[i].nextEntry.compare_exchange_strong(
+						savedNext, (long) Entry::markFrozen(savedNext));
+				savedNext = chunk->entriesArray[i].nextEntry.load();
 			}
 
-			uint128 savedWord = entriesArray[i].keyData;
+			uint128 savedWord = chunk->entriesArray[i].keyData;
 			while (!Entry::isFrozen(savedWord)) {
-				Utils::InterlockedCompareExchange128(&(entriesArray[i].keyData),
-						savedWord, Entry::markFrozen(savedWord));
-				savedWord = entriesArray[i].keyData;
+				Utils::InterlockedCompareExchange128(
+						&(chunk->entriesArray[i].keyData), savedWord,
+						Entry::markFrozen(savedWord));
+				savedWord = chunk->entriesArray[i].keyData;
 			}
 		}
 	}
 
-	void stabilizeChunk(Chunk *chunk);
-	RecovType freezeDecision(Chunk *chunk);
+	void stabilizeChunk(Chunk *chunk) {
+		long maxKey = LONG_MAX;
+		find(chunk, maxKey);		// implicitly removes all the delete entries
+		for (int i = 0; i < MAX; i++) {
+			long key = chunk->entriesArray[i].getKey();
+			Entry *next = chunk->entriesArray[i].nextEntry.load();
+			if ((key != Utils::DEFAULT_KEY) && !Entry::isDeleted(next)) {
+				if (!find(chunk, key))
+					insertEntry(chunk, chunk->entriesArray[i], key);
+			}
+		}
+	}
+
+	RecovType freezeDecision(Chunk *chunk) {
+		Entry *e = chunk->head->nextEntry.load();
+		int cnt = 0;
+		while (Entry::clearFrozen((long) e) != 0) {
+			cnt++;
+			e = e->nextEntry.load();
+		}
+
+		if (cnt == MIN)
+			return MERGE;
+
+		if (cnt == MAX)
+			return SPLIT;
+
+		return COPY;
+	}
+
 	Chunk *findMergeSlave(Chunk *chunk);
-	Chunk *freezeRecovery(Chunk *chunk, long key, TData data, RecovType recov,
-			Chunk *mergePartner, TriggerType trigger, bool *result);
+
+	Chunk *freezeRecovery(Chunk *oldChunk, long key, TData data,
+			RecovType recovType, Chunk *mergeChunk, TriggerType trigger,
+			bool *result) {
+		Chunk *retChunk = NULL, newChunk2 = NULL;
+
+		Chunk *newChunk1 = allocate();		// TODO: provide min and max
+
+		long separateKey;
+
+		switch (recovType) {
+		case COPY:
+			copyToOneChunk(oldChunk, newChunk1);
+			break;
+
+		case MERGE:
+			if ((getEntrNum(oldChunk) + getEntrNum(mergeChunk)) >= MAX) {
+				// Two chunks will be merged into two chunks
+				newChunk2 = allocate();
+				newChunk1->nextChunk = newChunk2;
+				newChunk2->nextChunk = NULL;
+				separateKey = mergeToTwoChunks(oldChunk, mergeChunk, newChunk1,
+						newChunk2);
+
+			} else {
+				mergeToOneChunk(oldChunk, mergeChunk, newChunk1);
+			}
+			break;
+
+		case SPLIT:
+			newChunk2 = allocate();
+			newChunk1->nextChunk = newChunk2;
+			newChunk2->nextChunk = NULL;
+			separateKey = splitIntoTwoChunks(oldChunk, newChunk1, newChunk2);
+			break;
+		}
+
+		helpInFreezeRecovery(newChunk1, newChunk2, key, separateKey, data,
+				trigger);
+
+		if (!atomic_compare_exchange_strong(&(oldChunk->newChunk), NULL,
+				newChunk1)) {
+			retireChunk(newChunk1);
+
+			if (newChunk2 != NULL)
+				retireChunk(newChunk2);
+
+			if (key < separateKey)
+				retChunk = oldChunk->newChunk.load();
+			else
+				retChunk = findChunk(key);
+
+		} else {
+			retChunk = NULL;
+		}
+
+		listUpdate(recovType, key, oldChunk);
+		return retChunk;
+	}
+
+	Chunk *helpInFreezeRecovery(Chunk *newChunk1, Chunk *newChunk2, long key,
+			long separateKey, TData data, TriggerType trigger);
+	void retireChunk(Chunk *chunk);
+	void listUpdate(RecovType recovType, long key, Chunk * chunk);
 };
 
 //} /* namespace lflist */
