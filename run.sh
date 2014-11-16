9827f1faaaf7d321430f799918a3c6fd38ea4da9
Sriram Patil <spsrirampatil@gmail.com>
2014-11-17 03:33:58 +0530

    Small change to test webhook

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf LFBTree
+#rm -rf LFBTree
 
 g++ -g3 -Wall -c -std=c++0x -MMD -MF"lflist/Chunk.d" -MT"lflist/Chunk.d" -o "lflist/Chunk.o" "./lflist/Chunk.cpp"
 g++ -o "LFBTree" ./lflist/Chunk.o -lpthread
